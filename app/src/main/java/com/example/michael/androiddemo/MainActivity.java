bc3d7fad0b2dbdc716ccda331588225173b804d1
zhaohongtao <zhaohongtao@letv.com>
2016-06-12 17:33:01 +0800

    custom textureView

--- app/src/main/java/com/example/michael/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/michael/androiddemo/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.michael.androiddemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+    private static final String TAG = "testSurfaceTexture";
+    private TextureView mTextureView;
+    private Surface mSurface;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+    private void initViews() {
+        mTextureView = (TextureView) findViewById(R.id.image_texture);
+        mTextureView.setSurfaceTextureListener(this);
+    }
+
+    public void setImageBitmap(View view) {
+        Log.i(TAG, "setImageBitmap");
+        if (mSurface != null) {
+            final Canvas canvas = mSurface.lockCanvas(null);
+            // draw bitmap
+            final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.thumbnail1);
+//            canvas.clipRect(0, 0, 360, 640);
+//            canvas.drawBitmap(bitmap, new Matrix(), new Paint());
+            canvas.drawBitmap(bitmap, null, new Rect(0, 0, 360, 640), new Paint());
+            canvas.save();
+
+
+
+
+
+
+
+
+
+
+
+
+            mSurface.unlockCanvasAndPost(canvas);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+        Log.i(TAG, "onSurfaceTextureAvailable");
+        mSurface = new Surface(surfaceTexture);
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        Log.i(TAG, "onSurfaceTextureDestroyed");
+        // TODO
+        return true;
+    }
+
+    // ====================================================================================
+    // will be never invoked
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+        Log.i(TAG, "onSurfaceTextureSizeChanged");
+        // do nothing
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+        Log.i(TAG, "onSurfaceTextureUpdated");
+        // do nothing
+    }
+    // ====================================================================================
+
 }
---
+++ app/src/main/java/com/example/michael/custom/CustomView.java
@@ -0,0 +1,33 @@
+package com.example.michael.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by michael on 16-6-6.
+ */
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+
+
+
+
+}
